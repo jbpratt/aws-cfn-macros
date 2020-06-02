08d2cde742680080f78e345803f4d6b05f545021
jbpratt78 <jbpratt78@gmail.com>
2020-06-01 23:27:56 -0500

    activator files and code

--- src/app.py
+++ src/app.py
@@ -1,8 +1,28 @@
-from typing import Dict
 import json
 
 
-def handler(event, _context) -> Dict[str, str]:
-    print(json.dumps(event))
+PREFIX = "SFDSSDK::"
 
-    return {"requestId": event["requestId"], "status": "success"}
+
+def handle_template(request_id, template):
+    for name, resource in template.get("Resources", {}).items():
+        if resource["Type"].startswith(PREFIX):
+            print(resource)
+            code = resource.get("Properties", {}).get("Code")
+            print(exec(code))
+
+
+def handler(event, context):
+    fragment = event["fragment"]
+    status = "success"
+
+    try:
+        fragment = handle_template(event["requestId"], event["fragment"])
+    except Exception as e:
+        raise e
+
+    return {
+        "requestId": event["requestId"],
+        "status": status,
+        "fragment": fragment,
+    }
