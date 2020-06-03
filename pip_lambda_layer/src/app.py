335b1fb29d27f402ce8f902fe1b6473c91f0e607
jbpratt78 <jbpratt78@gmail.com>
2020-06-02 21:49:33 -0500

    fix: s3 key was a large nested tree. Simplify to just the random string

--- pip_lambda_layer/src/app.py
+++ pip_lambda_layer/src/app.py
@@ -25,26 +25,26 @@ def handle_template(request_id, template):
             bucket = props["BucketName"]
 
             directory = tempfile.TemporaryDirectory().name
+
             packages_str = "".join([f'{x["name"]}=={x["version"]} ' for x in packages])
             subprocess.run(["pip", "install", packages_str, "-t", directory])
-            filename = os.path.join(
-                directory,
-                "".join(random.choice(string.ascii_lowercase) for i in range(10)),
-            )
-            shutil.make_archive(filename, "zip", directory)
 
+            filename = "".join(random.choice(string.ascii_lowercase) for i in range(10))
+            filename_w_dir = os.path.join(directory, filename)
+
+            shutil.make_archive(filename_w_dir, "zip", directory)
+
+            filename = filename + ".zip"
             try:
-                s3_client.upload_file(filename + ".zip", bucket, filename)
+                s3_client.put_object(Body=filename_w_dir, Bucket=bucket, Key=filename)
             except s3_client.exceptions.ClientError as err:
                 raise err
 
-            # zip directory
-            # upload to s3
             new_resources[name] = {
                 "Type": "AWS::Lambda::LayerVersion",
                 "Properties": {
                     "CompatibleRuntimes": props["CompatibleRuntimes"],
-                    "Content": {"S3Bucket": bucket, "S3Key": f"{filename}.zip"},
+                    "Content": {"S3Bucket": bucket, "S3Key": filename},
                     "LayerName": props["LayerName"],
                 },
             }
